d0b1d0b74cc4ad7cef886c0bcf1736b77a0b0a93
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-11-11 11:56:16 +0100

    Update firebase-core.sh

--- firebase-core.sh
+++ firebase-core.sh
@@ -6,3 +6,11 @@ python pompom.py \
 	--exclude out/gps-ads-17.2.1/dependencies.json \
 	-apv 28 \
 	poms deps zip
+
+python pompom.py \
+	--project-name firebase-core-16.0.9 \
+	--pom https://maven.google.com/com/google/firebase/firebase-core/16.0.9/firebase-core-16.0.9.pom \
+	--exclude out/gps-base-16.0.1/dependencies.json \
+	--exclude out/gps-ads-17.2.1/dependencies.json \
+	-apv 28 \
+	poms deps zip
